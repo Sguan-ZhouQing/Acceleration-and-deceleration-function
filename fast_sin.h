31ce376a7241d20cfc382726d1e8bf13d8c27c84
星必尘Sguan <148698322+Sguan-ZhouQing@users.noreply.github.com>
2025-11-05 12:21:43 +0800

    Add files via upload

---
+++ fast_sin.h
@@ -0,0 +1,12 @@
+#ifndef __FAST_SIN_H
+#define __FAST_SIN_H
+
+#include "data_parameters.h"
+
+float f1(float x);
+float f2(float x);
+float fast_sin(float x);
+float fast_cos(float x);
+void fast_sin_cos(float x, float *sin_x, float *cos_x);
+
+#endif // FAST_SIN_H
